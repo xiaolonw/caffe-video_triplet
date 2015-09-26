ab488142a7a68db43ebb6060af770903c3156ec1
Xiaolong Wang <xiaolonw@compute-1-1.local>
2015-09-26 00:14:18 -0400

    alexnet

---
+++ rank_scripts/rank_alexnet/train_video.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+TOOLS=/nfs/hn46/xiaolonw/video_cnncode2/caffe-video_triplet/build/tools
+
+$TOOLS/caffe train --solver=solver.prototxt 
+
+
+
+
+
+
+
+
--- rank_scripts/rank_gray_8m_1fc_pre2/train_video.sh
+++ rank_scripts/rank_gray_8m_1fc_pre2/train_video.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
-TOOLS=/nfs/hn46/xiaolonw/video_cnncode/caffe-video_rank/build_compute-0-5/tools
+TOOLS=/nfs/hn46/xiaolonw/video_cnncode2/caffe-video_triplet/build/tools
 
-GLOG_logtostderr=1 $TOOLS/finetune_net.bin /nfs/hn46/xiaolonw/video_cnncode/caffe-video_rank/scripts_video2/rank_gray_8m_1fc_pre2/imagenet_solver.prototxt /nfs/hn38/users/xiaolonw/video_models3/rank_gray_8m_1fc_pre2/bef_train_again/video__iter_435000
+$TOOLS/caffe train --solver=solver.prototxt --weights=/home/xiaolonw/video_models/unsup_vgg/video__iter_62809.caffemodel
+
+
+
+
+# /nfs/hn46/xiaolonw/video_cnncode/caffe-video_rank/scripts_video2/rank_gray_8m_1fc_pre2/imagenet_solver.prototxt /nfs/hn38/users/xiaolonw/video_models3/rank_gray_8m_1fc_pre2/bef_train_again/video__iter_435000
 
 
